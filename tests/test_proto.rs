7da7a1fdbcb7e172ba98fa11de90f7f0fea15821
marcel <marcel@aiperion.ai>
2024-07-09 09:08:59 +0300

    add protobuf serialization  - v.2

--- tests/test_proto.rs
+++ tests/test_proto.rs
@@ -1,9 +1,10 @@
 use protobuf::Message;
-use sketches_rust::{proto, DDSketch};
+
+use sketches_rust::{DDSketch, proto};
 
 #[test]
 pub fn test_proto() {
-    let mut initial_sketch = DDSketch::unbounded_dense(0.01).unwrap();
+    let mut initial_sketch = DDSketch::unbounded_dense(0.001).unwrap();
     let test_data = vec![0.1, 0.5, 0.7, 0.9, 1.4, 3.1, 0.6, 2.5, 0.55, 1.34, 5.34, 0.4, -1.4];
 
     for val in test_data {
@@ -13,6 +14,8 @@ pub fn test_proto() {
     let min = initial_sketch.get_min().unwrap();
     let max = initial_sketch.get_max().unwrap();
     let p50 = initial_sketch.get_value_at_quantile(0.5).unwrap();
+
+    println!("min = {:?} , max = {:?}, med = {:?}", min, max, p50);
 
     let sketch_proto = proto::ddsketch::DDSketch::from(initial_sketch);
     let bytes = sketch_proto.write_to_bytes().unwrap();
